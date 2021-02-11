3422d168dc1e87d1a967b3ba3ff2580deeddfcdd
fenchurch-nz <67564814+fenchurch-nz@users.noreply.github.com>
2021-02-11 13:55:51 +1300

    Add files via upload

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Living_High.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
