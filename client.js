69a332ef342acdb0ca1b62b59fe3b96efaf80a7a
fenchurch-nz <67564814+fenchurch-nz@users.noreply.github.com>
2021-02-11 16:28:26 +1300

    Update client.js

--- client.js
+++ client.js
@@ -1,7 +1,7 @@
 
 console.log('Client-side code running');
 
-const button = document.getElementById('myButton');
+const button = document.getElementById('submit');
 button.addEventListener('click', function(e) {
   console.log('button was clicked');
 
